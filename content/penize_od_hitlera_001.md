c7b3425356649a2d2cf62fc36e694a911b29bef5
adelaweidischova <48671822+adelaweidischova@users.noreply.github.com>
2021-08-18 19:46:41 +0000

    Deploying to docs from @ books-are-next/penize-od-hitlera@da3edfd6076db673c5757bcb0bf23e635343a409 🚀

--- content/penize_od_hitlera_001.md
+++
@@ -1,5 +0,0 @@
----
-title: ''
----
-
-![obalka_penize_od_hitlera.jpg](./resources/obalka_penize_od_hitle_fmt.jpeg)
---
+++ service-worker.js
@@ -0,0 +1,59 @@
+/* global self, caches, fetch */
+/* eslint-disable no-restricted-globals */
+
+const CACHE = 'cache-da3edfd';
+
+self.addEventListener('install', e => {
+  e.waitUntil(precache()).then(() => self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  self.clients
+    .matchAll({
+      includeUncontrolled: true,
+    })
+    .then(clientList => {
+      const urls = clientList.map(client => client.url);
+      console.log('[ServiceWorker] Matching clients:', urls.join(', '));
+    });
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE) {
+              console.log('[ServiceWorker] Deleting old cache:', cacheName);
+              return caches.delete(cacheName);
+            }
+            return null;
+          })
+        )
+      )
+      .then(() => {
+        console.log('[ServiceWorker] Claiming clients for version', CACHE);
+        return self.clients.claim();
+      })
+  );
+});
+
+function precache() {
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./index.html","./favicon.png","./manifest.json","./penize_od_hitlera_001.html","./penize_od_hitlera_002.html","./penize_od_hitlera_006.html","./penize_od_hitlera_007.html","./penize_od_hitlera_008.html","./penize_od_hitlera_009.html","./penize_od_hitlera_010.html","./penize_od_hitlera_011.html","./penize_od_hitlera_012.html","./penize_od_hitlera_013.html","./resources.html","./resources/image001_fmt.jpeg","./resources/image002_fmt.jpeg","./resources/index.xml","./resources/upoutavka_eknihy_fmt.jpeg","./resources/obalka_penize_od_hitle_fmt.jpeg","./scripts/bundle.js","./style/style.min.css"]));
+}
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.open(CACHE).then(cache => {
+      return cache.match(e.request).then(matching => {
+        if (matching) {
+          console.log('[ServiceWorker] Serving file from cache.');
+          console.log(e.request);
+          return matching;
+        }
+
+        return fetch(e.request);
+      });
+    })
+  );
+});
